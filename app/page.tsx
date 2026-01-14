f852a3d397f98a3519590430b0b50a3fbe2b18a4
Gabriella <gabriellamargarida18@gmail.com>
2026-01-14 16:51:36 -0300

    cria página de estátisticas do usuário

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
+import { StatsDashboard } from "../components/StatsDashboard";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm">
-        <h1 className="text-4xl font-bold text-center mb-4">
-          Bem-vindo ao Daylist WebApp
-        </h1>
-      </div>
+    <main className="min-h-screen bg-[#f9c7fa] flex items-center justify-center py-12 px-4">
+      <StatsDashboard />
     </main>
   );
 }
